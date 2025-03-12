cb4ef1833b11bbff77efa6947c7ebc13549c328d
iminurcomputerkid <juliennilesxd@gmail.com>
2025-03-11 21:22:35 -0400

    https integrated

--- server.py
+++ server.py
@@ -760,4 +760,9 @@ async def delete_secure_doc(username: str, doc_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", 
+    host="0.0.0.0",
+    port=443,
+    ssl_keyfile="key.pem",
+    ssl_certfile="cert.pem",
+    reload=True)
